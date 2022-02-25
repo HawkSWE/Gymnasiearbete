82848f24b1232eef98ed13ecb5255163907fa2c3
HawkSWE <83948065+HawkSWE@users.noreply.github.com>
2022-02-25 07:42:21 +0100

    Update Weather_Script.js

--- Weather_Script.js
+++ Weather_Script.js
@@ -6,6 +6,7 @@ $.get( "http://api.openweathermap.org/data/2.5/weather?q="+stad+"&appid=c5bf5d99
     let temp = Math.floor(data.main.temp)
     let tempmin = Math.floor(data.main.temp_min)
     let tempmax = Math.floor(data.main.temp_max)
+    let tempfeels = Math.floor(data.main.feels_like)
     let humidity = Math.floor(data.main.humidity)
     let windspeed = Math.floor(data.wind.speed)
     let imgicon = "http://openweathermap.org/img/wn/"+data.weather[0].icon+".png"
@@ -14,8 +15,11 @@ $.get( "http://api.openweathermap.org/data/2.5/weather?q="+stad+"&appid=c5bf5d99
     $("#temp").html(temp+" C");
     $("#mintemp").html(tempmin+" C");
     $("#maxtemp").html(tempmax+" C");
+    $("#feeltemp").html(tempfeels+" C");
     $("#humidity").html(humidity);
     $("#windspeed").html(windspeed+" m/s");
     $("#iconbild").attr('src', imgicon);
+
+    console.log(data);
 });
 
