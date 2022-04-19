c1809dbc48170b38e0e6569e9e817dded2022a83
HawkSWE <hook200364@gmail.com>
2022-04-19 10:56:27 +0200

    Update ISS.js

--- ISS.js
+++ ISS.js
@@ -4,10 +4,6 @@ $.get( "https://api.wheretheiss.at/v1/satellites/25544", function( data ) {
     let lat = Math.floor(data.longitude)
     let alti = Math.floor(data.altitude)
 
-    console.log(long);
-    console.log(lat);
-    console.log(alti);
-
     $("#long").html(long);
     $("#lat").html(lat);
     $("#höjd").html(alti);
