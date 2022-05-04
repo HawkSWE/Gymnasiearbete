fdee5d5d81aa40e16313274ad1c559e69f990a40
HawkSWE <hook200364@gmail.com>
2022-05-04 11:27:22 +0200

    Update Quiz.js

--- Quiz.js
+++ Quiz.js
@@ -1,8 +1,18 @@
+$.get( "https://the-trivia-api.com/api/questions", function( data ) {
 
+let fråga = data[0].question;
+let felsvar1 = data[0].incorrectAnswers[0];
+let felsvar2 = data[0].incorrectAnswers[1];
+let felsvar3 = data[0].incorrectAnswers[2];
+let rättsvar = data[0].correctAnswer;
 
-$.get( "https://opentdb.com/api.php?amount=1", function( data ) {
+console.log(fråga);
+console.log(felsvar1);
+console.log(felsvar2);
+console.log(felsvar3);
+console.log(rättsvar);
 
-console.log(data.results[0])
+$("#stadnamn").html(namn);
 
 
 });
